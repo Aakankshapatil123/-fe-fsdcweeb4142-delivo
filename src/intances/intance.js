4335f3e0bc14bca8cbe95c01dbdf247982759871
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-08-17 02:44:49 +0530

    fix configure frontend API URL for production

--- src/intances/intance.js
+++ src/intances/intance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: import.meta.env.VITE_API_URL + "/api/v1",
   timeout: 10000,
   withCredentials: true,
   headers: {
